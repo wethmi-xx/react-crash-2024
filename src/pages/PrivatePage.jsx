6b0bf81b3d5447214e3a0614f7d8f5b9ea56d72b
Wethmi Ranasinghe <wethmi@wethmi.local>
2026-05-12 16:52:29 +0530

    Adding private page

--- src/pages/PrivatePage.jsx
+++ src/pages/PrivatePage.jsx
@@ -1,9 +1,27 @@
-import Hero from "../components/Hero"
+import Hero from "../components/Hero";
+import {useState, useEffect} from 'react';
+
 
 const PrivatePage = () => {
+
+    const [names, setNames] = useState([]);
+
+    useEffect(() => {
+    const fetchNames = () => {
+        setNames(['Alice', 'Bob', 'Charlie']);
+    }
+    fetchNames();
+}, []);
+
+
     return (
         <>
         <Hero />
+        <ul>
+            {names.map((name, index) => (
+                <li key = {index}>{name}</li>
+            ))}
+        </ul>
         </>
     )
 };
